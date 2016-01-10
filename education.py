93d7bb107b9b6358e61b0bde1546f605f2ec90e9
jlybianto <jesse.lybianto@gmail.com>
2016-01-10 02:22:44 -0700

    Import necessary packages to obtain data

--- education.py
+++ education.py
@@ -2,8 +2,17 @@
 # IMPORT PACKAGES
 # ----------------
 
-
+# Beautiful Soup is a Python package designed for navigating, searching and modifying parse tree.
+# Requests is a package that allows download of data from any online resource.
+from bs4 import BeautifulSoup
+import requests
 
 # ----------------
 # OBTAIN DATA
 # ----------------
+
+# Import the page that contains the data to be scraped through requesting its URL
+url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content)
