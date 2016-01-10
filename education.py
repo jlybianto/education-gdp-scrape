008e15266419e542a4594ec4e209f21675148417
jlybianto <jesse.lybianto@gmail.com>
2016-01-10 15:59:43 -0700

    Use BeautifulSoup to isolate the relevant data within tables

--- education.py
+++ education.py
@@ -16,3 +16,13 @@ url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demo
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content)
+
+# Parse through content tables for relevant data.
+# Data is in table 7 (index 6) from 14 tables. Find all table rows ("tr").
+data = soup("table")[6].find_all("tr")
+# Data is in row 4 or index 3 // len(soup("table")[6].find_all("tr")) = 202
+data = data[3]
+# Data is in table 1 or index 0 // len(data("table")) = 2
+data = data("table")[0]
+# Filter all table rows ("tr") // len(data("table")[0]) = 375
+data = data("tr")
